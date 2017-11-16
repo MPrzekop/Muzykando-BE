9eae4f254ef0cf9d11979088a29c7b3fa30a1418
Michal Przekop <przekop.michal@gmail.com>
2017-11-16 20:25:36 +0100

    initial commit

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', '172.21.0.2');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'admin');
+define('_DB_PASSWD_', 'admin');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'O6QDosFAzjN6OVMIQ703iAfFAcDorsyfoTymagcfNRjXVQEF2fDBXLTT');
+define('_COOKIE_IV_', 'nOtdNefz');
+define('_PS_CREATION_DATE_', '2017-11-16');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.17');
+define('_RIJNDAEL_KEY_', 'p7GP9xfGU1VdB7a0IK29MjeiLN7xdcnq');
+define('_RIJNDAEL_IV_', 'obmugfr932bS05TA1WNSZg==');
