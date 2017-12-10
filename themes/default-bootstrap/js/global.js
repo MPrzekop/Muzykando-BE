7817ad36b274bac6417818a0cb4b1759d7481ce5
Michal Przekop <przekop.michal@gmail.com>
2017-12-10 22:24:04 +0100

    going further :)

--- themes/default-bootstrap/js/global.js
+++ themes/default-bootstrap/js/global.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
         }).mouseleave(function () {
         _final = new Date();
         _final = _final.setDate(_final.getDate());
-        var name = $(this).find("h5").innerHTML;
+        var name = $('.product-container .product-name' ).innerHTML;
         ga('send', 'timing', 'product in list', 'hover', (_final - _initial), name);
     });
 	$(document).on('change', 'select[name="manufacturer_list"], select[name="supplier_list"]', function(){
