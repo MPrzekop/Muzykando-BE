ad192a867ed4cf04970fd7661a7d8354ddbf68f9
Patryk Podworski <patrykpodworski@gmail.com>
2018-01-11 23:41:58 +0100

    Add module constructor.

--- modules/recommendations/recommendations.php
+++ modules/recommendations/recommendations.php
@@ -5,4 +5,23 @@ if (!defined('_PS_VERSION_')) {
  
 class Recommendations extends Module
 {
+    public function __construct()
+    {
+      $this->name = 'Recommendations';
+      $this->tab = 'advertising_marketing';
+      $this->version = '1.0.0';
+      $this->author = 'Ja';
+      $this->need_instance = 0;
+      $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+      $this->bootstrap = true;
+
+      parent::__construct();
+
+      $this->displayName = $this->l('Rekomendowane produkty');
+      $this->description = $this->l('Rekomendacje produktów dla użytkowników.');
+      $this->confirmUninstall = $this->l('Na pewno chcesz odinstalowac?');
+      if (!Configuration::get('MYMODULE_NAME')) {
+        $this->warning = $this->l('Brak nazwy');
+      }
+    }
 }
