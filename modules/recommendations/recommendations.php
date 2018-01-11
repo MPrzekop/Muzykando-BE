07e076193c65b1974ea2f09c8b2df37a653d9ac3
Patryk Podworski <patrykpodworski@gmail.com>
2018-01-11 23:56:50 +0100

    Add rest of files for recommendations plugin.

--- modules/recommendations/recommendations.php
+++ modules/recommendations/recommendations.php
@@ -24,4 +24,53 @@ class Recommendations extends Module
         $this->warning = $this->l('Brak nazwy');
       }
     }
+
+    public function install()
+    {
+    if (Shop::isFeatureActive())
+        Shop::setContext(Shop::CONTEXT_ALL);
+
+      return parent::install() &&
+        $this->registerHook('leftColumn') &&
+        $this->registerHook('header') &&
+        Configuration::updateValue('MYMODULE_NAME', 'Recommendations');
+    }
+
+    public function uninstall()
+    {
+      if (!parent::uninstall()) {
+        return false;
+      }
+       
+      return true;
+    }
+
+    public function hookDisplayLeftColumn($params)
+    {
+      $id = Context::getContext()->customer->id;
+    $recommendationsJson = file_get_contents("http://172.20.83.78:9090/user/$id/recommendations");
+    $recommendationsArray = json_decode($recommendationsJson, true);
+    $ids = "";
+    if(count($recommendationsArray) > 0){
+    foreach ($recommendationsArray as $key => $value) {
+      $ids .= $value['itemID'].",";
+    }
+        $ids = substr($ids, 0, strlen($ids)-1);
+        $recommendedProducts = Db::getInstance()->executeS('SELECT * FROM '._DB_PREFIX_.'product WHERE id IN ('.$ids.')');
+          $this->smarty->assign(array(
+              'products' => $recommendedProducts,
+          ));
+      }
+      return $this->display(__FILE__, 'product-list.tpl')
+    //   return $this->display(__FILE__, 'recommendations.tpl');
+    }
+    public function hookDisplayRightColumn($params)
+    {
+      return $this->hookDisplayLeftColumn($params);
+    }
+    public function hookDisplayHeader()
+    {
+      $this->context->controller->addCSS($this->_path.'css/mymodule.css', 'all');
+    }
+    
 }
