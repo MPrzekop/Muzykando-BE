ab061571b0b659bb8d8b1c842540b7d917f7f1c5
Patryk Podworski <patrykpodworski@gmail.com>
2018-01-19 01:38:30 +0100

    Add displaying recommended products.

--- modules/homefeatured/homefeatured.php
+++ modules/homefeatured/homefeatured.php
@@ -118,50 +118,45 @@ class HomeFeatured extends Module
 	}
 	public function hookDisplayHomeTab($params)
 	{
-		if (!$this->isCached('tab.tpl', $this->getCacheId('homefeatured-tab'))){
+		if (!$this->isCached('tab.tpl', $this->getCacheId('homefeatured-tab')))
 			$this->_cacheProducts();
-			
-			}
 		return $this->display(__FILE__, 'tab.tpl', $this->getCacheId('homefeatured-tab'));
 	}
 	public function hookDisplayHome($params)
 	{
 		if (!$this->isCached('homefeatured.tpl', $this->getCacheId()))
-		{
-		//	$this->_cacheProducts();
-		
-$id = Context::getContext()->customer->id;
-//echo "AA".$id;
-$recommendationsJson = file_get_contents("http://172.20.83.77:8080/recommends/user/$id", true);
-//$recommendationsJson = "";
-//echo $recommendationsJson;
-$recommendationsArray = json_decode($recommendationsJson, true);
-$ids = "";
-if(count($recommendationsArray) > 0){
-foreach ($recommendationsArray as $key => $value) {
-  $ids .= $value['itemID'].",";
+		{	}	
+		$id = Context::getContext()->customer->id;
+
+		//reading data from mahout
+		$responseJson = json_decode(file_get_contents("http://172.20.83.77:8080/recommends/user/".$id, true));
+		$productIds = array();
+
+		foreach ($responseJson as $item) {
+			$productIds[] = $item['itemID'];
+		}
+
+		// preparing array of products
+		$recommendedProducts = array();	
+	for($i = 0; $i < count($productIds); $i++){
+		$recommendedProducts[$i] = (array)(new Product($productIds[$i], false, '1'));
+		$recommendedProducts[$i]['price_without_reduction'] = '';
+		$recommendedProducts[$i]['id_image'] = Product::getCover((int)$productIds[$i])['id_image'];
+		$recommendedProducts[$i]['link'] = Context::getContext()->link->getProductLink(
+			(int)$productIds[$i], 
+			$recommendedProducts[$i]['link_rewrite'], 
+			$recommendedProducts[$i]['category'], 
+			$recommendedProducts[$i]['ean13']);
 }
-//$ids = "100,245,234,";
-$ids = substr($ids, 0, -1);
-$ids2 = explode(',' , $ids);
-$recommendedProducts2 = array();
-    $recommendedProducts = Db::getInstance()->executeS('SELECT * FROM '._DB_PREFIX_.'product WHERE id_product IN ('.$ids.')');
-	for($i = 0; $i < count($ids2); $i++){
-		$recommendedProducts2[$i] = (array)(new Product($ids2[$i], false, '1'));
-		$recommendedProducts2[$i]['price_without_reduction'] = '';
-		$recommendedProducts2[$i]['id_image'] = Product::getCover((int)$ids2[$i])['id_image'];
-		$recommendedProducts2[$i]['link'] = Context::getContext()->link->getProductLink((int)$ids2[$i], $recommendedProducts2[$i]['link_rewrite'], $recommendedProducts2[$i]['category'], $recommendedProducts2[$i]['ean13']);
-}
-//var_dump($recommendedProducts2);
+// assigning products to display
 	$this->smarty->assign(
 				array(
-					'products' =>(object) $recommendedProducts2,
+					'products' =>(object) $recommendedProducts,
 					'add_prod_display' => Configuration::get('PS_ATTRIBUTE_CATEGORY_DISPLAY'),
 					'homeSize' => Image::getSize(ImageType::getFormatedName('home')),
 				)
 			);
-		}
-}
+
 		return $this->display(__FILE__, 'homefeatured.tpl', $this->getCacheId());
 	}
 	public function hookDisplayHomeTabContent($params)
